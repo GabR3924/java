ece23da0184602bbe9cc263cc9e4cfe09c848a8f
gabr3924 <garc2004@gmail.com>
2023-10-23 13:59:11 -0400

    22

--- src/com/example/Funciones.java
+++ src/com/example/Funciones.java
@@ -10,6 +10,13 @@ public class Funciones {
         //opcion2: funcion sin parametro y con tipo de retorno
             String menu = getMenu();
             System.out.println(menu);
+
+        //opcion3: funcion con parametron y sin tipo de retorno
+            imprimirSaludo("OpenBootcamp");
+
+        //Opcion 4 : Funcion con parametros y  con tipo de retorno
+        String saludo = obtenerSaludo("gabriela", "Rodriguez");
+        System.out.println(saludo);
     }
 
     //void es un tipo de dato que indica que no devuelve nada...
@@ -24,5 +31,13 @@ public class Funciones {
     static String getMenu(){
         return "Bienvenidos al Ecommerce de skincare: \n 1 - ver Skincare";
         // \n es un salto de linea, tipo <br/> en js
+    }
+
+    static void imprimirSaludo(String nombre) {
+        System.out.println("Buenas tardes " + nombre);
+    }
+
+    static String obtenerSaludo(String nombre, String apellido){
+        return "Buenos tardeees " + nombre + " " + apellido;
     }
 }
