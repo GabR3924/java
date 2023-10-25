4a7a9826f43fd857660d8242cf7dffd3416e789d
gabr3924 <garc2004@gmail.com>
2023-10-24 22:15:06 -0400

    jajas

---
+++ src/com/example/estructurasControl/condicionales/repeticion/Switch.java
@@ -0,0 +1,21 @@
+package com.example.estructurasControl.condicionales.repeticion;
+
+public class Switch {
+
+    public static void main(String[] args) {    
+        String dia = "Lunes";
+
+        switch (dia){
+            case "Lune":
+                System.out.println("Mano tengo fe");
+                break;
+            case "Martes":
+                System.out.println("Tengo sueno");
+                break;
+            case "Lunes":
+                System.out.println("Quiero ver arcane");
+                break;
+            default:
+        }
+    }
+}
